ac570960fa8de7429cb07bf5d7a072fca018db92
manseon <91870110+full-fish@users.noreply.github.com>
2022-10-03 18:30:16 +0900

    update url

--- client/src/services/map.js
+++ client/src/services/map.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 // let url = "http://localhost:8000";
-let url = "http://ec2-13-209-14-109.ap-northeast-2.compute.amazonaws.com";
+let url = "http://ec2-52-79-217-69.ap-northeast-2.compute.amazonaws.com";
 export async function getMap() {
   const res = await axios.get(`${url}/api/map`, { withCredentials: true });
   return res;
--- client/src/services/sideBarLeft.js
+++ client/src/services/sideBarLeft.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 // let url = "http://localhost:8000";
-let url = "http://ec2-13-209-14-109.ap-northeast-2.compute.amazonaws.com";
+let url = "http://ec2-52-79-217-69.ap-northeast-2.compute.amazonaws.com";
 export async function getSideBarLeft() {
   const res = await axios.get(`${url}/api/sideBarLeft`, { withCredentials: true });
   return res;
